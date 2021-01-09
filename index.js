97b0788ba386718b44f905fd0797baeed76a2b65
Jay <mantine08@gmail.com>
2021-01-08 23:02:43 -0800

    Changed to use config vars

--- index.js
+++ index.js
@@ -17,14 +17,11 @@ app.use(express.json()); //Allow passing of json files
 const uri = process.env.ATLAS_URI;
 
 //connect with uri
-mongoose.connect(
-  "mongodb+srv://jay:!HOz8o7Hkx4N@shopple-8dmxr.mongodb.net/shopple",
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 const connection = mongoose.connection;
 
