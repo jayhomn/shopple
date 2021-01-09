0c5d4f78dee3a80c2c6553bdf29e50883c27ea63
Jay <mantine08@gmail.com>
2021-01-09 00:11:31 -0800

    Added paths requirement

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const { default: Axios } = require("axios");
